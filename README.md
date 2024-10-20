f17bcb95e03475a2650296d9d6a12e214ac016bb
oeyoews <jyao4783@gmail.com>
2024-10-20 19:44:59 +0800

    refactor: to sidebar

--- README.md
+++ README.md
@@ -14,3 +14,7 @@
 `F5`
 
 * configuration: https://code.visualstudio.com/api/references/contribution-points#contributes.configuration
+
+## Credits
+
+https://github.com/microsoft/vscode-extension-samples/blob/main/webview-view-sample
--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,24 @@
 import * as vscode from 'vscode';
-import { notify } from './notify';
+import { usewikiViewProvider } from './webviews';
+
+import usewikiCmd, { usewikiCli } from './usewikiCmd';
+import openWikiCmd, { openWikiCli } from './openWikiCmd';
+import wikiInfoCmd, { wikiInfoCli } from './commands/wikiInfo';
 
 export async function activate(context: vscode.ExtensionContext) {
-  const provider = new MyWebviewViewProvider(context);
+  const provider = new usewikiViewProvider(context);
   context.subscriptions.push(
-    vscode.window.registerWebviewViewProvider('ollamaView', provider)
+    vscode.window.registerWebviewViewProvider('usewiki2', provider)
   );
-}
+  const disposable = vscode.commands.registerCommand(usewikiCli, usewikiCmd);
+  const openDisposable = vscode.commands.registerCommand(
+    openWikiCli,
+    openWikiCmd
+  );
+  const wikiInfoDisposable = vscode.commands.registerCommand(
+    wikiInfoCli,
+    wikiInfoCmd
+  );
 
-export class MyWebviewViewProvider implements vscode.WebviewViewProvider {
-  private _view?: vscode.WebviewView;
-  constructor(
-    private context: vscode.ExtensionContext,
-    private _extensionUri = context.extensionUri
-  ) {}
-
-  resolveWebviewView(
-    webviewView: vscode.WebviewView,
-    context: vscode.WebviewViewResolveContext,
-    _token: vscode.CancellationToken
-  ) {
-    this._view = webviewView;
-    webviewView.webview.options = {
-      enableScripts: true,
-      localResourceRoots: [this._extensionUri],
-    };
-
-    webviewView.webview.html = this.getWebviewContent(webviewView.webview);
-
-    webviewView.webview.onDidReceiveMessage(async (message) => {
-      switch (message.type) {
-        case 'sendWiki':
-          vscode.window.showInformationMessage(message.data.text);
-          break;
-        case 'showMessage':
-          vscode.window.showInformationMessage(message.data.text);
-          break;
-      }
-    });
-  }
-  private getWebviewContent(webview: vscode.Webview) {
-    const scriptUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, 'res', 'main.js')
-    );
-    const styleMainUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, 'res', 'main.css')
-    );
-    const nonce = getNonce();
-    return `
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-	      <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Custom Sidebar</title>
-	              <link href="${styleMainUri}" rel="stylesheet">
-            </head>
-            <body>
-                <h3>输入框</h3>
-                <input type="text" id="inputField" placeholder="在这里输入..." />
-                <button class="send-wiki">提交</button>
-
-	          <script nonce="${nonce}" src="${scriptUri}"></script>
-            </body>
-            </html>
-        `;
-  }
-}
-
-function getNonce() {
-  let text = '';
-  const possible =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < 32; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
+  context.subscriptions.push(disposable, openDisposable);
 }
