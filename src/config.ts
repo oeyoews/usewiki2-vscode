25de9f9dd29ce135a2a0893b50e419603114e259
oeyoews <jyao4783@gmail.com>
2024-10-22 18:05:38 +0800

    fix: update default tag

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import * as vscode from 'vscode';
 export const config = vscode.workspace.getConfiguration('usewiki2');
 
 export const enableHttps = () => config.get('enableHttps', false);
-export const defaultTag = () => config.get('defaultTag', 'Journalc');
+export const defaultTag = () => config.get('defaultTag', 'Journal');
 export const defaultUsername = () => config.get('defaultUsername', '');
 export const getIp = () => config.get('ip', 'localhost');
 export const getPort = () => config.get('port', 8080);
